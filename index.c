754dd5b39c9e6357a75d7f45bc4ce20b7ef95fa6
dudakrug <dudakrugamaral@gmail.com>
2026-01-28 13:21:26 -0300

    add a while loop and fixed else if logic issues

--- index.c
+++ index.c
@@ -9,32 +9,52 @@ int balance (int victories, int defeats){
 
 int main() {
     
-    //Input
-    int victories = 0;
-    int defeats = 0;
-    int result = 0;
-    char rankResult[50] = " ";
+    while(1){
+        //Input
+        int victories = 0;
+        int defeats = 0;
+        int balanceResult = 0;
+        char rankResult[50];
 
-    printf("\nType how much victories you have: ");
-    scanf("%d", &victories);
-    getchar();
+        printf("\n\nType how much victories you have: ");
+        scanf("%d", &victories);
+        getchar();
 
-    printf("Type how much defeats you have: ");
-    scanf("%d", &defeats);
-    getchar();
+        printf("Type how much defeats you have: ");
+        scanf("%d", &defeats);
+        getchar();
 
-    //Process
-    result = balance(victories, defeats);
+        //Process
+        balanceResult = balance(victories, defeats);
 
-        //rankResult
-        if(result < 10){
-            strcpy(rankResult, "Iron"); 
-        }
+            //rankResult
+            if(balanceResult <= 10){
+                strcpy(rankResult, "Iron"); 
+            } 
+            else if(11 <= balanceResult && balanceResult <= 20){
+                strcpy(rankResult, "Bronze"); 
+            }
+            else if(21 <= balanceResult && balanceResult <= 50){
+                strcpy(rankResult, "Silver"); 
+            }
+            else if(51 <= balanceResult && balanceResult <= 80){
+                strcpy(rankResult, "Gold"); 
+            }
+            else if(81 <= balanceResult && balanceResult <= 90){
+                strcpy(rankResult, "Diamond"); 
+            }
+            else if(91 <= balanceResult && balanceResult <= 100){
+                strcpy(rankResult, "Legendary"); 
+            }
+            else if(balanceResult >= 101){
+                strcpy(rankResult, "Imortal");
+            }
 
+        
+        //Output
+        printf("\nYour balance of victories is: %d", balanceResult);
+        printf("\nYour ranked is: %s", rankResult);
+    }
     
-    //Output
-    printf("\nYour balance of victories is: %d", result);
-    printf("\nYour ranked is: %s", rankResult);
-
     return 0;
 }
